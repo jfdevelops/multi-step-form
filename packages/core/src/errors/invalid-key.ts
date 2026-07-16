53e536d114c4c12d48c19630717b65519da9f8d4
Joey Finkel <61193959+joeyfinkel@users.noreply.github.com>
2026-07-15 23:40:16 -0400

    refactor(errors): migrate invalid key error

--- packages/core/src/errors/invalid-key.ts
+++ packages/core/src/errors/invalid-key.ts
@@ -1,63 +1,31 @@
-export namespace InvalidKeyError {
-  export type defaultMessages = {
-    invalid: string;
-    valid: string;
-  };
-  export type keys<TInvalid extends unknown[], TValid extends unknown[]> = {
-    invalid: TInvalid;
-    valid: TValid;
-  };
-  export type options<TInvalid extends unknown[], TValid extends unknown[]> = {
-    messages: defaultMessages;
-    keys: keys<TInvalid, TValid>;
-    formatter: Intl.ListFormat;
-  };
+import {
+  createMultiStepFormError,
+  type MultiStepFormErrorContext,
+} from './multi-step-form-error.js';
+
+const scope = 'invalidKey' as const;
+
+export interface InvalidKeyContext
+  extends MultiStepFormErrorContext<typeof scope> {
+  invalidKeys: unknown[];
+  validKeys: unknown[];
 }
-export class InvalidKeyError<
-  invalidKeys extends unknown[],
-  validKeys extends unknown[]
-> extends Error {
-  constructor(
-    invalidKeys: invalidKeys,
-    validKeys: validKeys,
-    message?:
-      | string
-      | ((options: InvalidKeyError.options<invalidKeys, validKeys>) => string)
-  ) {
+
+export class InvalidKeyError extends createMultiStepFormError(
+  { code: 'invalidKey', scope },
+)(
+  (scope, { invalidKeys, validKeys }: InvalidKeyContext) => {
     const formatter = new Intl.ListFormat('en', {
       style: 'long',
       type: 'conjunction',
     });
-    const defaultInvalidMessage = `Invalid keys were found (${formatter.format(
-      invalidKeys.map(String)
+    const invalidMessage = `Invalid keys were found (${formatter.format(
+      invalidKeys.map(String),
     )}). Please remove them to continue.`;
-    const defaultValidMessage = `The available keys are ${formatter.format(
-      validKeys.map(String)
+    const validMessage = `The available keys are ${formatter.format(
+      validKeys.map(String),
     )}`;
-    let resolvedMessage = `${defaultInvalidMessage} ${defaultValidMessage}`;
 
-    if (typeof message === 'string') {
-      resolvedMessage = message;
-    }
-
-    if (typeof message === 'function') {
-      resolvedMessage = message({
-        formatter,
-        keys: {
-          invalid: invalidKeys,
-          valid: validKeys,
-        },
-        messages: {
-          invalid: defaultInvalidMessage,
-          valid: defaultValidMessage,
-        },
-      });
-    }
-
-    super(resolvedMessage);
-
-    this.name = 'InvalidKeyError';
-
-    Object.setPrototypeOf(this, InvalidKeyError.prototype);
-  }
-}
+    return `[${scope}]: ${invalidMessage} ${validMessage}`;
+  },
+) {}
